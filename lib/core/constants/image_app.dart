e3df833fd6006f7d3dc5d7f4e0c190b467e19bb2
Ayoub-kraamui <ayoob775453026@gmail.com>
2026-02-13 01:03:21 +0300

    feat: convert image app png to webp.

--- lib/core/constants/image_app.dart
+++ lib/core/constants/image_app.dart
@@ -1,13 +1,13 @@
 class ImageApp {
-  static const String profile = 'assets/images/profile.jpg';
+  static const String profile = 'assets/images/profile.webp';
   static const String intelligentMeasurementsExtractionSystem =
-      'assets/images/intelligent_measurements_extraction_system.png';
+      'assets/images/intelligent_measurements_extraction_system.webp';
   static const String yemeniUniversities =
-      'assets/images/yemeni_universities.png';
-  static const String eCommerceApplication = 'assets/images/e-commerce.png';
+      'assets/images/yemeni_universities.webp';
+  static const String eCommerceApplication = 'assets/images/e-commerce.webp';
   static const String inventoryManagementSystem =
-      'assets/images/inventory_management_system.png';
-  static const String coffeeApp = 'assets/images/coffee_app.png';
-  static const String qaydApp = 'assets/images/qayd.png';
-  static const String noteApp = 'assets/images/note_app.png';
+      'assets/images/inventory_management_system.webp';
+  static const String coffeeApp = 'assets/images/coffee_app.webp';
+  static const String qaydApp = 'assets/images/qayd.webp';
+  static const String noteApp = 'assets/images/note_app.webp';
 }
